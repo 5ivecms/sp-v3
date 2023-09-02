7d6e89d56efd358838e34fde0dd74e055238f6fb
5ivecms <greatseo@yandex.ru>
2023-09-02 12:41:43 +0300

    обновление nestjs

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,13 @@ import { Logger, LoggerErrorInterceptor } from 'nestjs-pino'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  //const app = await NestFactory.create(AppModule, { bufferLogs: true })
   const app = await NestFactory.create(AppModule, { logger: console })
+
   app.useGlobalPipes(new ValidationPipe())
   app.useLogger(app.get(Logger))
   app.useGlobalInterceptors(new LoggerErrorInterceptor())
-  const server = await app.listen(process.env.SERVER_PORT || 5000)
-  //server.setTimeout(120 * 1000)
+
+  await app.listen(process.env.SERVER_PORT || 5000)
 }
-//AppClusterService.clusterize(bootstrap)
+
 bootstrap()
